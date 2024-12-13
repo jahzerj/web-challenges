ceb1085aa6e2b9ea36b783b94ae60f3e70d84cee
Jahaziel <jahzerahj@gmail.com>
2024-12-13 16:46:21 +0100

    finish challenge

--- js-basics-and-variables_lifetime-calculator/index.js
+++ js-basics-and-variables_lifetime-calculator/index.js
@@ -20,3 +20,22 @@ Hint: You can combine text and variables using the `+` operator.
 const currentAge = Number(process.argv[2]); // This takes the `<age>` entered in the command `node index.js <age>`.
 
 console.log("Your current age is: " + currentAge);
+
+const daysSoFar = 365 * currentAge;
+const daysRemain = 80 * 365 - daysSoFar;
+const averageDays = 80 * 365;
+const percentageLived = (daysSoFar / averageDays) * 100;
+const daysSlept = (daysSoFar * 8) / 24;
+
+console.log("You have lived: ", daysSoFar, "days so far.");
+console.log(
+  "You have : ",
+  daysRemain,
+  " days left in your life based on the average life expectancy of 80 years old."
+);
+console.log("You have lived for: ", percentageLived, " % of your expected days.");
+console.log(
+  "You have slept for ",
+  daysSlept,
+  "days, based on an average of 8 hours of sleep per night."
+);
