8fbc3f2eeb6426d1a85294a644b3efdf4d01671b
Jahaziel <jahzerahj@gmail.com>
2024-12-16 16:26:36 +0100

    finish section 4

--- js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans_statements/js/index.js
@@ -5,17 +5,42 @@ const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
 const receivedPassword = "password1234";
 
+if (receivedPassword=== SUPER_SECRET_PASSWORD) {
+    console.log('Welcome! You are logged in as Brunhilde1984.')
+} else {
+    console.log('Access denied!')
+}
 // Part 2: Even / Odd
 const number = 6;
-
+if (number % 2 === 0) {
+    console.log('even number') 
+} else {
+    console.log('odd number')
+}
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 99;
+let price = 0;
+
+if (numberOfHotdogs >= 1000000) {
+    price = numberOfHotdogs * 0.1;
+    console.log(price);
+} else if (numberOfHotdogs >= 100) {
+    price = numberOfHotdogs * 1;
+    console.log(price);
+} else if (numberOfHotdogs >= 5) {
+    price = numberOfHotdogs * 1.5;
+    console.log(price);
+} else { 
+    price = numberOfHotdogs * 2;
+    console.log(price);
+}
 
 // Part 4: Daytime
-const currentHour = 12;
+const currentHour = 120;
 
-const statement = "";
+let statement = "";
 
+currentHour > 17 ?  statement = "Partytime!!!": statement = "Still need to learn...";
 console.log(statement);
 
 // Part 5: Greeting
