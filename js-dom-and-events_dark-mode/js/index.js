ff95ba12f95c8a6543673eba74ded6af8f090ab0
Jahaziel <jahzerahj@gmail.com>
2024-12-16 15:45:06 +0100

    start challenge

--- js-dom-and-events_dark-mode/js/index.js
+++ js-dom-and-events_dark-mode/js/index.js
@@ -1,3 +1,20 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+// const bodyElement = document.querySelector('[data-js="root"]');
+const addButton = document.querySelector('[data-js="dark-mode-button"]');
+const removeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+addButton.addEventListener("click", () => {
+    bodyElement.classList.add("dark");
+});
+
+removeButton.addEventListener("click", () => {
+    bodyElement.classList.remove("dark");
+});
+
+toggleButton.addEventListener("click", () => {
+    bodyElement.classList.toggle("dark");
+});
