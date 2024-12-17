a270ff75b32792104dfcf1680694c4c0106126a2
Jahaziel <jahzerahj@gmail.com>
2024-12-17 15:35:35 +0100

    finish challenge

--- js-functions_functions-with-parameters-extra/js/index.js
+++ js-functions_functions-with-parameters-extra/js/index.js
@@ -22,8 +22,12 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function `printCircumference`.
 
-// printCircumference(4);
-// printCircumference(6);
+function printCircumference(radius){
+   let circumference = 2 * Math.PI * radius;
+   console.log(circumference);
+}
+printCircumference(4);
+printCircumference(6);
 
 /*
 3: Write a function that accepts the width and length of a rectangle
@@ -34,6 +38,10 @@ printSquare(5);
 */
 
 // Uncomment the following function calls and implement the function `printRectangleArea`.
+function printRectangleArea(width,length){
+   const area = width * length;
+   console.log('The area of the rectangle is', area);
+}
+printRectangleArea(5, 7);
+printRectangleArea(3, 4);
 
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4);
