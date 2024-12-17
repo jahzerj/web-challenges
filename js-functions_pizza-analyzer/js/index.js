a23e448fb81d9bc5e79525eddabf66d8aadf2852
Jahaziel <jahzerahj@gmail.com>
2024-12-17 21:06:33 +0100

    finish task1

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,16 +9,23 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  pizzaInput.value = pizzaSize1;
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  pizzaInput2.value = pizzaSize2;
 });
 
 // Task 1
 // define the function calculatePizzaGain here
+function calculatePizzaGain(diameter1, diameter2) {
+  const area1 = Math.PI * (diameter1 / 2) ** 2;
+  const area2 = Math.PI * (diameter2 / 2) ** 2;
+  const pizzaGain = ((area2 - area1) / area1) * 100;
+  document.querySelector('data-js="output"').innerHTML = Math.round(pizzaGain);
+}
 
+calculatePizzaGain(diameter1, diameter2);
 // Task 2
 // define the function updatePizzaDisplay here
 
