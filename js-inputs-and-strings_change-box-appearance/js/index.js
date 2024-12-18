328b35308052be6759390be4bc654f22f52a0d91
Jahaziel <jahzerahj@gmail.com>
2024-12-18 16:48:25 +0100

    make first slider work

--- js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,15 @@
 console.clear();
+
+const inputColor = document.querySelector('[data-js="input-color"]');
+const inputRotation = document.querySelector('[data-js="input-rotation"]');
+const inputRadius = document.querySelector('[data-js="input-radius"]');
+const box = document.querySelector('[data-js="box"]');
+
+inputColor.addEventListener("input", () => {
+  box.style.backgroundColor = `hsl(${inputColor.value} 50% 50%)`;
+  console.log(inputColor.value);
+});
+
+console.log(typeof inputColor);
+
+console.log("number", number);
