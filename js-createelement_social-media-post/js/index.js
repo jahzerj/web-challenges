550084bac51fac000435b63f1f176244484f3c6c
Jahaziel <jahzerahj@gmail.com>
2024-12-20 13:37:20 +0100

    finished post

--- js-createelement_social-media-post/js/index.js
+++ js-createelement_social-media-post/js/index.js
@@ -11,6 +11,14 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
 
+
+const body = document.querySelector('body');
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Social Media Post';
+
+
+
 //Create the post add class
 const post = document.createElement('article');
 post.setAttribute("class", "post");
@@ -20,14 +28,30 @@ const content = document.createElement('p');
 content.setAttribute("class","post__content");
 content.textContent = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
 
+
+
+//Create the footer  add class
 const footer = document.createElement('footer');
 footer.setAttribute("class","post__footer");
+
+//Create the span add classes
 const span =document.createElement('span');
 span.setAttribute("class","post__username");
-span.textContent('@username');
+span.textContent= '@username';
+
+
+//Create the button add classes
 const button =document.createElement('button');
 button.setAttribute("type","button");
 button.setAttribute("class","post__button");
 button.setAttribute("data-js","like_button");
-button.textContent('♥ Like');
+button.textContent = '♥ Like';
+
+footer.append(span,button);
+post.append(content,footer);
+
+
+body.append(post)
+
+
 
