538500c2016ab3c7674d0cc52764a5de2d5b587a
Jahaziel <jahzerahj@gmail.com>
2025-01-07 17:05:05 +0100

    finished filter section

--- js-array-methods/js-array-methods_fun-with-flags/index.js
+++ js-array-methods/js-array-methods_fun-with-flags/index.js
@@ -4,23 +4,35 @@ import { Country } from "./components/Country/Country.js";
 const container = document.querySelector('[data-js="card-container"]');
 const queryInput = document.querySelector('[data-js="query-input"]');
 
+// queryInput.addEventListener("input", (event) => {
+//   container.innerHTML = "";
+
+//   const searchString = event.target.value;
+
+//   const foundCountry = countries.find(function(country) {
+//     return country.name.toLowerCase().startsWith(searchString);
+
+//   })
+//   console.log(foundCountry)
+
+//   if (foundCountry) {
+//     const countryElement = Country(foundCountry);
+//     container.append(countryElement);
+//   }
+// });
+
 queryInput.addEventListener("input", (event) => {
-  event.preventDefault();
   container.innerHTML = "";
-  
-  const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData);
 
-  console.log(data)
+  const searchString = event.target.value.toLowerCase();
 
+  const foundCountries = countries.filter(function (country) {
+    return country.name.toLowerCase().startsWith(searchString);
+  });
+  console.log(foundCountries);
 
-  const searchString = event.target.value;
-
-  const foundCountry = null;
-
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
+  foundCountries.forEach(function (country) {
+    const countryElement = Country(country);
     container.append(countryElement);
-  }
+  });
 });
-
