5e8b9fd3db649169a804c6315ef75b57740eccbc
Jahaziel <jahzerahj@gmail.com>
2025-01-08 16:50:44 +0100

    added pentagon

--- js-structure/js-structure_shapes/index.js
+++ js-structure/js-structure_shapes/index.js
@@ -1,14 +1,24 @@
 import { getRandomColor } from "./utils/randomColor.js";
+import Circle from "./components/Circle/Circle.js";
 
 console.clear();
 
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = getRandomColor();
-});
+
+
+const circleElement = Circle();
+const pentagonElement = Pentagon();
+
+
+function Pentagon() {
+  const pentagon = document.createElement("div");
+  pentagon.classList.add("pentagon");
+  pentagon.addEventListener("click", () => {
+    pentagon.style.backgroundColor = getRandomColor();
+  });
+  return pentagon;
+}
 
 const square = document.createElement("div");
 square.classList.add("square");
@@ -16,10 +26,5 @@ square.addEventListener("click", () => {
   square.style.backgroundColor = getRandomColor();
 });
 
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor();
-});
 
-root.append(circle, square, pentagon);
+root.append(circleElement, square, pentagonElement);
