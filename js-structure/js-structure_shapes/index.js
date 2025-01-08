7a1bf5eaf76f0d0d17beec5cb4b0ebe6248fabf5
Jahaziel <jahzerahj@gmail.com>
2025-01-08 14:01:18 +0100

    import works

--- js-structure/js-structure_shapes/index.js
+++ js-structure/js-structure_shapes/index.js
@@ -1,4 +1,4 @@
-import { getRandomColor } from "./utils/randomColor";
+import { getRandomColor } from "./utils/randomColor.js";
 
 console.clear();
 
@@ -7,19 +7,19 @@ const root = document.getElementById("root");
 const circle = document.createElement("div");
 circle.classList.add("circle");
 circle.addEventListener("click", () => {
-  circle.style.backgroundColor = "#ccc";
+  circle.style.backgroundColor = getRandomColor();
 });
 
 const square = document.createElement("div");
 square.classList.add("square");
 square.addEventListener("click", () => {
-  square.style.backgroundColor = "#ccc";
+  square.style.backgroundColor = getRandomColor();
 });
 
 const pentagon = document.createElement("div");
 pentagon.classList.add("pentagon");
 pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = "#ccc";
+  pentagon.style.backgroundColor = getRandomColor();
 });
 
 root.append(circle, square, pentagon);
