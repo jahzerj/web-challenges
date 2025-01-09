205bbe37917c2dc3bad8940b83e52ae0f581ba1a
Jahaziel <jahzerahj@gmail.com>
2025-01-09 16:39:52 +0100

    fix part2

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,22 +2,22 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-  for (i= 1; i <= 5; i++) {
+  for (i = 1; i <= 5; i++) {
     let star = document.createElement("img");
-    star.setAttribute("src", "./assets/star-empty.svg")
-    starContainer.append(star)
-    
-    if (filledStars <= i) {
-      star.setAttribute("src", "/ass")
-    } 
+    star.setAttribute("src", "./assets/star-empty.svg");
 
+    starContainer.append(star);
+
+    star.setAttribute(
+      "src",
+      i <= filledStars ? "./assets/star-filled.svg" : "./assets/star-empty.svg"
+    );
+    // --^-- write or modify code above this line --^--
   }
-  // --^-- write or modify code above this line --^--
 }
-
-renderStars();
+renderStars(4);
