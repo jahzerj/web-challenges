27eb967f347e88e3494cde444edbf32bd997ffea
Jahaziel <jahzerahj@gmail.com>
2025-01-09 16:22:57 +0100

    part 2

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -7,9 +7,15 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-  for (i= 1; i < 5; i++) {
+  for (i= 1; i <= 5; i++) {
     let star = document.createElement("img");
     star.setAttribute("src", "./assets/star-empty.svg")
+    starContainer.append(star)
+    
+    if (filledStars <= i) {
+      star.setAttribute("src", "/ass")
+    } 
+
   }
   // --^-- write or modify code above this line --^--
 }
