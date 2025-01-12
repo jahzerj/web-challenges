5855bbdcb59cf1c60941c7fbd1a44829adf39fa2
Jahaziel <jahzerahj@gmail.com>
2025-01-12 17:08:55 +0100

    pedantic on index.js

--- js-structure/js-structure_quiz-app/index.js
+++ js-structure/js-structure_quiz-app/index.js
@@ -1,10 +1,4 @@
-import {cards} from './utils/cards.js'
-import { Header } from './components/Header/Header.js';
-import { Form } from './components/Form/Form.js';
-import { Bookmark } from './components/Bookmark/Bookmark.js';
-import { CardList } from './components/CardList/Cardlist.js';
 import { App } from './components/App/App.js';
-import formHandle from './utils/formsubmit.js';
 
 
 // The render function renders the app to the DOM
