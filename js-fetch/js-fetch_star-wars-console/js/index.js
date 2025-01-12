ab4f481a4334c2ac28a7f42100a4ada31fb8e389
Jahaziel <jahzerahj@gmail.com>
2025-01-12 17:36:32 +0100

    start starwars console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,14 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+//Function created to fetch data, async because we don't know how long the call takes
+ async function fetchData() {
+    //calls api, and waits for the fetch to work, converts the information to varial response
+    const response  = await fetch("url");
+    //turns response into a json, data stores that information
+    const data = await response.json();
+    return data;
+ }
+console.log(data)
 
 fetchData();
