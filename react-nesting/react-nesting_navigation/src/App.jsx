1fe1e115cd1e17be792570d5c1f0fedcfd05b7a9
Jahaziel <jahzerahj@gmail.com>
2025-01-15 16:56:31 +0100

    working on challenge

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -2,25 +2,23 @@ import "./styles.css";
 
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
+import Header from "./components/Header";
+import Naviation from "./components/Navigation";
+import Link from "./components/Link";
+// import Image from "./components/Image";
 
 export default function App() {
   return (
     <>
-      <header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
-        <nav>
-          <a className="navigation__link" href="#home">
-            Home
-          </a>
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#impressum">
-            Impressum
-          </a>
-        </nav>
+      <Header>
+        <Link href="#">
+          <img className="round-img" src={logo} alt="logo" />
+        </Link>
+        <Naviation>
+          <Link href="#home">Home</Link>
+          <Link href="#about">About</Link>
+          <Link href="#impressum">Impressum</Link>
+        </Naviation>
         <button
           type="button"
           onClick={() => console.log("I could toggle a profile!")}
@@ -28,7 +26,7 @@ export default function App() {
         >
           <img className="round-image" src={avatar} alt="avatar" />
         </button>
-      </header>
+      </Header>
       <main>content goes here…</main>
     </>
   );
