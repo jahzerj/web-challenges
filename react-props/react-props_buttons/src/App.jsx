292b69e80f5c5417a804e7e68206e3470df8d7d3
Jahaziel <jahzerahj@gmail.com>
2025-01-15 13:26:18 +0100

    finished challenge

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,17 +1,26 @@
 export default function App() {
+  function handleClick() {
+    console.log("My click is sensitive");
+  }
   return (
     <Button
       color={"orange"}
-      disabled={true}
+      disabled={false}
       text={"I am a button"}
       fontsize={"14px"}
+      onClick={handleClick}
     />
   );
 }
 
-function Button({ color, disabled, text, fontsize }) {
+function Button({ color, disabled, text, fontsize, onClick }) {
   return (
-    <button disabled={disabled} color={color} fontsize={fontsize}>
+    <button
+      disabled={disabled}
+      color={color}
+      fontSize={fontsize}
+      onClick={onClick}
+    >
       {text}
     </button>
   );
