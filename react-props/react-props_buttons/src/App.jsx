82f3f98af85f7a96acd7806d93d00ae16b877030
Jahaziel <jahzerahj@gmail.com>
2025-01-15 13:07:02 +0100

    created button

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,3 +1,18 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <Button
+      color={"orange"}
+      disabled={true}
+      text={"I am a button"}
+      fontsize={"14px"}
+    />
+  );
+}
+
+function Button({ color, disabled, text, fontsize }) {
+  return (
+    <button disabled={disabled} color={color} fontsize={fontsize}>
+      {text}
+    </button>
+  );
 }
