ec031bd55a813154769af3cd34af5876bb405040
Jahaziel <jahzerahj@gmail.com>
2025-01-16 00:15:38 +0100

    finish

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -1,31 +1,22 @@
 import "./styles.css";
 
-import avatar from "./img/avatar.jpg";
-import logo from "./img/logo.jpg";
 import Header from "./components/Header";
 import Naviation from "./components/Navigation";
 import Link from "./components/Link";
-// import Image from "./components/Image";
+import Avatar from "./components/Avatar";
+import Logo from "./components/Logo";
 
 export default function App() {
   return (
     <>
       <Header>
-        <Link href="#">
-          <img className="round-img" src={logo} alt="logo" />
-        </Link>
+        <Logo />
         <Naviation>
           <Link href="#home">Home</Link>
           <Link href="#about">About</Link>
           <Link href="#impressum">Impressum</Link>
         </Naviation>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
+        <Avatar />
       </Header>
       <main>content goes here…</main>
     </>
