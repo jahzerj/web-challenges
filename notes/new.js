bfbba4062c8456d91f3c3da8a18e56e78b910b0e
Jahaziel <jahzerahj@gmail.com>
2025-01-16 12:43:25 +0100

    download mockinterview challenge

---
+++ notes/new.js
@@ -0,0 +1,16 @@
+console.log('hello world!')
+
+const words = ["Jaws", "Up", "Alien", "Gravity", "Inception", "Psycho"];
+
+function findLongestWord(words) {
+  // TODO: return the longest word
+}
+
+// Step 0. intialize longestString = 'Inception'
+// Step 1. Loop through each string of [words] with end condition using words.length
+// Step 2. check length of each string in words
+// Step 3. compare length of string vs longestString
+// Step 4. store longer string in longestString if string.length() > longestString
+// Step 5. return longestString
+// 
+// //
