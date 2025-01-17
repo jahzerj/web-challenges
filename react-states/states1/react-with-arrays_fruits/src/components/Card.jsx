27efd648496748ec55317f0bbe1d9ce274b26719
Jahaziel <jahzerahj@gmail.com>
2025-01-17 15:59:29 +0100

    finished challenge and bonus

--- react-states/states1/react-with-arrays_fruits/src/components/Card.jsx
+++ react-states/states1/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,5 +1,8 @@
 import "./Card.css";
+//include color prop in the Card function so it can be used from the array
+export default function Card({ name, color }) {
+  const colorClass = `card--${color.toLowerCase()}`;
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+  return <p className={`card ${colorClass}`}>{name}</p>;
+  //This is how you can apply the colorClass to each card using props
 }
