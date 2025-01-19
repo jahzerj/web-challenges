65647b72f40087525b3b0ad3c572e440fda1bad8
Jahaziel <jahzerahj@gmail.com>
2025-01-19 19:22:04 +0100

    finished challenge

--- react-with-arrays/react-with-arrays_users/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_users/src/components/Card.jsx
@@ -1,13 +1,13 @@
-import "./Card.css";
-import Tag from "./Tag";
-import Button from "./Button";
+import './Card.css';
+import Tag from './Tag';
+import Button from './Button';
 function Card({ user }) {
   return (
     <article className="card">
       <h2>{user.name}</h2>
       <ul className="card__taglist">
         {user.roles.map((role) => (
-          <li key={user.id}>
+          <li key={role}>
             <Tag tag={role} />
           </li>
         ))}
