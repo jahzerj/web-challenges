879bc0a146e1df2c9cdab2be865f6949457a1530
Jahaziel <jahzerahj@gmail.com>
2025-01-28 16:35:47 +0100

    working on challenge

---
+++ js-testing/js-unit-testing_people/index.js
@@ -0,0 +1,27 @@
+export function getFirstNames(people) {
+  return people.map((person) => person.firstName);
+}
+
+export function getFullNames(people) {
+  return people.map((person) => `${person.lastName} (${person.lastName})`);
+}
+export function getNameAndAge(people) {
+  return people.map((person) => `${person.lastName} (${person.age})`);
+}
+
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => person.age === age);
+}
+
+export function getPeopleNamesOlderThan(people, age) {
+  return people
+    .filter((person) => person.age > age)
+    .map((person) => `${person.firstName} ${person.lastName}`);
+}
+export function getPeopleByLastName(people, lastName) {}
+
+export function findPersonById(people, id) {}
+
+export function isAnyoneOlderThan(people, age) {}
+
+export function getFullNamesSortedByAge(people) {}
