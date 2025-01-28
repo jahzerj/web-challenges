5fc7fc2dcc76b0529c1405964f778e1733ff67f8
Jahaziel <jahzerahj@gmail.com>
2025-01-28 12:18:53 +0100

    added dynamic style

--- react-styled-components/react-styled-components_box/pages/index.js
+++ react-styled-components/react-styled-components_box/pages/index.js
@@ -1,10 +1,11 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import { BoxWithStyledComponents } from "@/components/BoxWithStyledComponents";
 
 export default function HomePage() {
   return (
     <div>
-      <BoxWithClassName />
-      <BoxWithClassName isBlack />
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents $isBlack />
     </div>
   );
 }
