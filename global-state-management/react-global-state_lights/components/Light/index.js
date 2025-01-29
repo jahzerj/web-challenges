6aed2d9ea0cdea13506ab53c286c542db91b1fa2
Jahaziel <jahzerahj@gmail.com>
2025-01-29 16:49:58 +0100

    working on lightsApp

---
+++ global-state-management/react-global-state_lights/components/Light/index.js
@@ -0,0 +1,20 @@
+import { LightButton, Icon, Text, Name, State } from "./Light.styled";
+
+export default function Light({ light, onToggle }) {
+  return (
+    <LightButton
+      type="button"
+      onClick={() => {
+        onToggle(lightId);
+      }}
+      $isOn={light.isOn}
+    >
+      <Icon $isOn={light.isOn}>💡</Icon>
+      <Text>
+        <Name>{light.name}</Name>
+        {/* dont need this state? */}
+        <State>{isOn ? "On" : "Off"}</State>
+      </Text>
+    </LightButton>
+  );
+}
