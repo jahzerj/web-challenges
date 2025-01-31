36d548e52ca09cf323b56c4350871c9b4e6abab8
Jahaziel <jahzerahj@gmail.com>
2025-01-31 14:39:06 +0100

    added global count

--- global-state-management/react-global-state_lights/pages/_app.js
+++ global-state-management/react-global-state_lights/pages/_app.js
@@ -1,21 +1,23 @@
-import GlobalStyle from "../styles";
-import Layout from "../components/Layout";
+import GlobalStyle from '../styles';
+import Layout from '../components/Layout';
 
-import { useState } from "react";
+import { useState } from 'react';
 
 const initialLights = [
-  { id: 1, name: "Living Room", isOn: false },
-  { id: 2, name: "Kitchen", isOn: false },
-  { id: 3, name: "Bedroom", isOn: false },
-  { id: 4, name: "Bathroom", isOn: false },
-  { id: 5, name: "Garage", isOn: false },
-  { id: 6, name: "Porch", isOn: false },
-  { id: 7, name: "Garden", isOn: false },
-  { id: 8, name: "Office", isOn: false },
+  { id: 1, name: 'Living Room', isOn: false },
+  { id: 2, name: 'Kitchen', isOn: false },
+  { id: 3, name: 'Bedroom', isOn: false },
+  { id: 4, name: 'Bathroom', isOn: false },
+  { id: 5, name: 'Garage', isOn: false },
+  { id: 6, name: 'Porch', isOn: false },
+  { id: 7, name: 'Garden', isOn: false },
+  { id: 8, name: 'Office', isOn: false },
 ];
+const initialCount = 0;
 
 export default function App({ Component, pageProps }) {
   const [lights, setLights] = useState(initialLights);
+  const [count, setCount] = useState(initialCount);
 
   function handleToggle(lightId) {
     const newLightState = lights.map((light) =>
@@ -24,10 +26,17 @@ export default function App({ Component, pageProps }) {
     setLights(newLightState);
   }
 
+  const lightsOnCount = lights.filter((light) => light.isOn).length;
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} lights={lights} handleToggle={handleToggle} />
+      <Component
+        {...pageProps}
+        lights={lights}
+        handleToggle={handleToggle}
+        lightsOnCount={lightsOnCount}
+      />
     </Layout>
   );
 }
