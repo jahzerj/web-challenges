0da8f05b910937e59dd210bb1a67dae84971c4d0
Jahaziel <jahzerahj@gmail.com>
2025-01-31 12:34:57 +0100

    fixed isOn state component

--- global-state-management/react-global-state_lights/components/Light/index.js
+++ global-state-management/react-global-state_lights/components/Light/index.js
@@ -1,5 +1,4 @@
-import { LightButton, Icon, Text, Name, State } from "./Light.styled";
-import { LightButton, Icon, Text, Name, State } from "./Light.styled";
+import { LightButton, Icon, Text, Name, State } from './Light.styled';
 
 export default function Light({ light, onToggle }) {
   return (
@@ -14,10 +13,7 @@ export default function Light({ light, onToggle }) {
       <Icon $isOn={light.isOn}>💡</Icon>
       <Text>
         <Name>{light.name}</Name>
-        {/* dont need this state? */}
-
-        {/* <State>{isOn ? "On" : "Off"}</State> */}
-        <State>{$isOn={light.isOn} ? "On" : "Off"}</State>
+        <State>{light.isOn ? 'On' : 'Off'}</State>
       </Text>
     </LightButton>
   );
