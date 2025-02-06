b3ae9cc05f8206eb0616bb19056be90084fd131e
Jahaziel <jahzerahj@gmail.com>
2025-02-06 17:06:23 +0100

    finish backend basic challenges

--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,15 @@
-import { createServer } from "node:http";
+import { createServer } from 'node:http';
+import Chance from 'chance';
 
-// export const server = …
+export const server = createServer((request, response) => {
+    const person = { first-name: "jimmy"}
+  const chance = new Chance();
+  const name = chance.name();
+  const age = chance.age();
+  const profession = chance.profession();
+
+  response.setHeader('Content-type', 'application/json');
+  response.end(
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}`
+  );
+});
