13e4cbb0a5a577c665ea9fcf554e0f7e53f0bdd7
Jahaziel <jahzerahj@gmail.com>
2025-02-06 11:34:51 +0100

    finished backend challenge_routes

--- backend-basics/backend-basics_routes/server.js
+++ backend-basics/backend-basics_routes/server.js
@@ -1,3 +1,14 @@
-import { createServer } from "node:http";
+import { createServer } from 'node:http';
 
-// export const server = …
+export const server = createServer((request, response) => {
+  if (request.url === '/api/fish/1') {
+    response.statusCode = 200;
+    response.end('Shrimp');
+  } else if (request.url === '/api/fish/2') {
+    response.statusCode = 200;
+    response.end('Anemone');
+  } else {
+    response.statusCode = 400;
+    response.end('Not Found');
+  }
+});
