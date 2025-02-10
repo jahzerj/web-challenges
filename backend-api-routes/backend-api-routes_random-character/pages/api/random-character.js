8256aa97d801eb6074b1514896fe1a5d8f42c1a4
Jahaziel <jahzerahj@gmail.com>
2025-02-10 13:58:00 +0100

    finished challenge

--- backend-api-routes/backend-api-routes_random-character/pages/api/random-character.js
+++ backend-api-routes/backend-api-routes_random-character/pages/api/random-character.js
@@ -3,8 +3,8 @@ import Chance from 'chance';
 export default function handler(request, response) {
   const chance = new Chance();
   const character = {
-    firstName: chance.first();
-    lastName: chance.last();
-  }
+    firstName: chance.first(),
+    lastName: chance.last(),
+  };
   response.status(200).json(character);
 }
