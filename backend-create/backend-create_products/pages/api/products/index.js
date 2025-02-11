8dd9b630b95bcfc598fdbbcb921dc4f575280161
Jahaziel <jahzerahj@gmail.com>
2025-02-11 16:31:53 +0100

    finished challenge

--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -1,15 +1,40 @@
-import dbConnect from "@/db/connect";
-import Product from "@/db/models/Product";
+import dbConnect from '@/db/connect';
+import Product from '@/db/models/Product';
 
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
+  if (request.method === 'GET') {
     const products = await Product.find();
-
     response.status(200).json(products);
     return;
   }
 
-  response.status(405).json({ status: "Method not allowed." });
+  if (request.method === 'POST') {
+    try {
+      const productData = request.body;
+      console.log('productData', productData);
+
+      await Product.create(productData);
+
+      response.status(200).json({ status: 'Product created' });
+      return;
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
+  }
+  response.status(405).json({ status: 'Method not allowed' });
 }
+
+// export default async function handler(request, response) {
+//   await dbConnect();
+
+//   if (request.method === "GET") {
+//     const products = await Product.find();
+
+//     response.status(200).json(products);
+//     return;
+//   }
+
+//   response.status(405).json({ status: "Method not allowed." });
+// }
